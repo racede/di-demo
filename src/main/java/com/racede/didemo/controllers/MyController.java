bb010565ca1f14952e2d592ba2cd7c3954d3046f
Ricardo Acedo de Talavera <ricardo.acedo@gruposantander.com>
2019-09-05 12:19:23 +0200

    Initial commit

---
+++ src/main/java/com/racede/didemo/controllers/MyController.java
@@ -0,0 +1,11 @@
+package com.racede.didemo.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MyController {
+
+  public void hello(){
+    System.out.println("Hello!");
+  }
+}
